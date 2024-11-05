1c6ffdeef14535313e9abf718c1912512a2942af
Xander White <xander.belov23@yandex.ru>
2024-11-05 17:13:10 +0300

    font и favicon

--- get_images.php
+++ get_images.php
@@ -1,5 +1,5 @@
 <?php
 $directory = './images/gallery';
-$images = glob($directory . "/*.{jpg,jpeg,png,gif,webp}", GLOB_BRACE);
+$images = glob($directory . "/*.{jpg,jpeg,png,gif,webp,JPG}", GLOB_BRACE);
 echo json_encode($images);
 ?>
