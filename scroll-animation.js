d0f1f083e7e9b8d33788376351db56a4db55a606
Xander White <xander.belov23@yandex.ru>
2024-12-17 00:29:20 +0300

    rotate service on scroll

--- scroll-animation.js
+++ scroll-animation.js
@@ -1,12 +1,19 @@
 // Получаем элемент
 const elements = document.querySelectorAll(".how__row");
 const introContainer = document.querySelector(".intro__content__container");
+const services = document.querySelectorAll(".service");
 
-window.addEventListener("scroll", ()=>{checkHow();
-	checkInto();});
+window.addEventListener("scroll", () => {
+	checkHow();
+	checkIntro();
+	checkService();
+});
 
-window.addEventListener("load",()=>{checkInto();})
+window.addEventListener("load", () => {
+	checkIntro();
+});
 
+//Проверяем высоту блока how
 function checkHow() {
 	elements.forEach((element) => {
 		const rect = element.getBoundingClientRect();
@@ -25,7 +32,8 @@ function checkHow() {
 	});
 }
 
-function checkInto() {
+//Проверяем высоту блока intro
+function checkIntro() {
 	const rect = introContainer.getBoundingClientRect();
 
 	if (rect.top * 1.05 <= window.innerHeight) {
@@ -33,4 +41,25 @@ function checkInto() {
 	} else {
 		introContainer.classList.remove("active");
 	}
+}
+
+//Проверяем высоту карточек service
+function checkService() {
+	services.forEach((s) => {
+		const rect = s.getBoundingClientRect();
+
+		if (window.innerWidth >= 990 || window.innerWidth < 450) {
+			if (rect.top + 200 <= window.innerHeight) {
+				s.classList.remove("rotated");
+			} else {
+				s.classList.add("rotated");
+			}
+		}else{
+			if (rect.top + 200 <= window.innerHeight) {
+				s.classList.remove("show-back");
+			} else {
+				s.classList.add("show-back");
+			}
+		}
+	});
 }
